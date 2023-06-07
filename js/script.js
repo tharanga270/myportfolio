c221ead6ca98fff88bc6d062b94165e7c1eb46a2
tharanga270 <developerwordpress270@gmail.com>
2023-06-07 20:04:38 +0530

    product inner pages done with responsive

--- js/script.js
+++ js/script.js
@@ -21,4 +21,28 @@ $(function() {
 	        },
 	    }
 	});
+
+	// owl-finefood-carousel
+	$('.owl-cmn-inner-pages-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    autoplay:true,
+	    autoplayTimeout:1500,
+	    autoplayHoverPause:true,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        500:{
+	            items:1
+	        },
+	        800:{
+	            items:2
+	        },
+	        1300: {
+	        	items: 3
+	        },
+	    }
+	});
 });
