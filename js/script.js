090e5a26fae6e363634e0d905ca6871115557c56
tharanga270 <developerwordpress270@gmail.com>
2023-06-09 12:39:32 +0530

    inner pages content update and scroll animation done

--- js/script.js
+++ js/script.js
@@ -76,4 +76,32 @@ $(function() {
         delay: 10,
         time: 2000
     });
+
+     /* Animations on Scroll */
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animate__animated animate__backInLeft');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animate__animated animate__backInRight');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animate__animated animate__fadeInDown');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-5').waypoint(function (direction) {
+        $('.js--wp-5').addClass('animate__animated animate__pulse');
+    }, {
+        offset: '50%'
+    });
+
 });
